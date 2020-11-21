96f1d2a1d43b471712a2ca30f4fa160bf61bb6f8
ryan.bollettino@gmail.com <bollo@bollo-laptop.localdomain>
2020-11-21 13:01:21 -0600

    commit tree 3a2602fd77a738aee668383338430d30f4ef5b0e

--- lib/min.rb
+++ lib/min.rb
@@ -9,5 +9,10 @@ require 'pry'
 # }
 
 def get_the_min(groceries)
+  #groceries.values.min.flatten 
+      ##^^^ORDER! finds values, then minimum in those arrays
+      #and then flattens the list, (which was three arrays one one value each)
+groceries.values.flatten.min
+    #^^ values, puts them all in ONE array, then finds minimum
   #code your solution here!
 end
